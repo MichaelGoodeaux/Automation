349045fcc11c986248a025cf35367d1ad1bcb905
Michael Goodeaux <79286128+MichaelGoodeaux@users.noreply.github.com>
2022-03-11 18:35:50 -0600

    Update Manual-DeleteTempFiles.ps1

--- Manual-DeleteTempFiles.ps1
+++ Manual-DeleteTempFiles.ps1
@@ -1,5 +1,4 @@
-﻿#This code deletes .tmp, .xls, and .xlsx files generated within the C:\Windows\Temp directory
-#This is intended for use in automating Maintenance activity by Cloud Operations personnel. 
+﻿#This code deletes .tmp, .xls, and .xlsx files generated within the C:\Windows\Temp directory 
 #WARNING: This script is destructive and proper backups should take place before this script is run
 
 Get-ChildItem -Path 'C:\Windows\Temp\*' -include *.tmp,*xls,*xlsx | foreach { Remove-Item -Path $_.FullName }
