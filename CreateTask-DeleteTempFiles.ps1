fd626d8b47a8325176f32df213aee665de99e254
Michael Goodeaux <79286128+MichaelGoodeaux@users.noreply.github.com>
2022-03-19 13:36:18 -0500

    Update CreateTask-DeleteTempFiles.ps1

--- CreateTask-DeleteTempFiles.ps1
+++ CreateTask-DeleteTempFiles.ps1
@@ -1,14 +1,15 @@
-﻿#This script generates and schedules a powershell script that deletes .tmp, .xls, and .xlsx files generated within the C:\Windows\Temp directory
+#This script generates and schedules a powershell script that deletes .tmp, .xls, and .xlsx files generated within the C:\Windows\Temp directory
+#This is intended for use in automating Maintenance activity by Cloud Operations personnel. 
 #WARNING: This script creates a destructive automation. Ensure proper backups are in place.
   
-#$Path is where the generated file will live. Ideally C:\Staging
-	$Path = ''
+#$Path is where the generated file will live. Ideally C:\Staging\
+	$Path = '#path'
 	$FileName = 'DSP_DeleteTempFiles.ps1'
 	$FullPath = $Path + $FileName
 	$DeleteTemp = "Get-ChildItem -Path 'C:\Windows\Temp\*' -include *.tmp,*xls,*xlsx | foreach { Remove-Item -Path $_.FullName }"
 	$DeleteTemp | Out-File $FullPath
 	
-	$action = New-ScheduledTaskAction -Execute $FullPath
+	$action = New-ScheduledTaskAction -Execute "PowerShell.exe" -Argument "-File $fullpath" 
 	$Principal = New-ScheduledTaskPrincipal -GroupID "BUILTIN\Administrators" -RunLevel Highest
 	$trigger = New-ScheduledTaskTrigger -Weekly -DaysOfWeek Sunday -At 6AM
 	$Settings = New-ScheduledTaskSettingsSet -ExecutionTimeLimit (New-TimeSpan -Hours 2)
